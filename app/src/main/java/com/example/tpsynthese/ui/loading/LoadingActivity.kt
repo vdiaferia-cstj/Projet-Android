ec814f8fac8489bff57b1c6d4d10822b7a7c6798
Vincent Diaferia <1967480@cstj.qc.ca>
2023-04-25 15:20:05 -0400

    Started LoadingActivity

--- app/src/main/java/com/example/tpsynthese/ui/loading/LoadingActivity.kt
+++ app/src/main/java/com/example/tpsynthese/ui/loading/LoadingActivity.kt
@@ -1,4 +1,12 @@
 package com.example.tpsynthese.ui.loading
 
-class LoadingActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tpsynthese.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
 }
