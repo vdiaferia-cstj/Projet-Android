ae18d9a1dae00a229b79088ee4cc118b9edb3748
Vincent Diaferia <1967480@cstj.qc.ca>
2023-05-02 13:44:23 -0400

    Started QR code

--- app/src/main/java/com/example/tpsynthese/ui/tickets/detail/TicketsViewModel.kt
+++ app/src/main/java/com/example/tpsynthese/ui/tickets/detail/TicketsViewModel.kt
@@ -3,8 +3,13 @@ package com.example.tpsynthese.ui.tickets.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class TicketsViewModel : ViewModel() {
+
+    private val _ticketUiState = MutableStateFlow<TicketsUiState>(TicketsUiState.Empty)
+    val ticketUiState = _ticketUiState.asStateFlow()
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
