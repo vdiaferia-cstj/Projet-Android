78dc229991221a53651046f54180d878f8526606
jeremy desjardins <2043120@cstj.qc.ca>
2023-05-17 23:03:57 -0400

    dans le milieu du gateway detail

--- app/src/main/java/com/example/tpsynthese/ui/gateways/detail/GatewaysFragment.kt
+++ app/src/main/java/com/example/tpsynthese/ui/gateways/detail/GatewaysFragment.kt
@@ -9,9 +9,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.tpsynthese.R
+import com.example.tpsynthese.core.ColorHelper
 import com.example.tpsynthese.databinding.FragmentGatewayBinding
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 
@@ -34,9 +38,24 @@ class GatewaysFragment : Fragment(R.layout.fragment_gateway) {
                     Toast.LENGTH_LONG
                 ).show()
                 is GatewaysUiState.Loading -> TODO()
-                is GatewaysUiState.Success ->
+                is GatewaysUiState.Success -> {
+                    binding.GatewayChipStatus.text = it.gateway.connection.status
+                    binding.GatewayChipStatus.chipBackgroundColor = ColorHelper.connectionStatusColor(binding.root.context,it.gateway.connection.status)
+                    Glide.with(binding.imvGateway1).load(it.gateway.config.kernel[0]).into(binding.imvGateway1)
+                    Glide.with(binding.imvGateway2).load(it.gateway.config.kernel[1]).into(binding.imvGateway2)
+                    Glide.with(binding.imvGateway3).load(it.gateway.config.kernel[2]).into(binding.imvGateway3)
+                    Glide.with(binding.imvGateway4).load(it.gateway.config.kernel[3]).into(binding.imvGateway4)
+                    Glide.with(binding.imvGateway5).load(it.gateway.config.kernel[4]).into(binding.imvGateway5)
+                    binding.txvDbm.text = it.gateway.connection.signal.toString()
+                    binding.txvLatency.text = it.gateway.connection.signal.toString()
+                    binding.txvDownloadGateway.text = it.gateway.connection.download.toString()
+                    binding.txvUploadGateway.text = it.gateway.connection.upload.toString()
+                    binding.txvIp.text = it.gateway.connection.ip
+                    binding.txvMac.text = it.gateway.serialNumber
+                    binding.txvPin.text = it.gateway.pin
+                }
             }
-        }
+        }.launchIn(lifecycleScope)
     }
 
 
