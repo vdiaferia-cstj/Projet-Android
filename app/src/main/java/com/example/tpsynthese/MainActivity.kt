79316b126d38727e6688134f956350b685111b4c
jeremy desjardins <2043120@cstj.qc.ca>
2023-05-18 22:56:28 -0400

    fin de la partie b

--- app/src/main/java/com/example/tpsynthese/MainActivity.kt
+++ app/src/main/java/com/example/tpsynthese/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
     }
 
     companion object {
